dfa47ab144b8436d427759564be835888b738613
aayaresko <aayaresko@gmail.com>
2018-02-16 16:34:25 +0200

    added getter.

--- src/Entity/Currency.php
+++ src/Entity/Currency.php
@@ -27,6 +27,14 @@ class Currency
     /**
      * @return mixed
      */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
     public function getName()
     {
         return $this->name;
