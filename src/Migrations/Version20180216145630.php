f99fada53165fc924580ad9a8789c178f8b93eb1
aayaresko <aayaresko@gmail.com>
2018-02-16 16:56:44 +0200

    added migration.

--- src/Migrations/Version20180216145630.php
+++ src/Migrations/Version20180216145630.php
@@ -8,15 +8,14 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20180216143849 extends AbstractMigration
+class Version20180216145630 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE rate (id INT AUTO_INCREMENT NOT NULL, currency_id INT NOT NULL, value NUMERIC(8, 4) NOT NULL, created_at DATETIME NOT NULL, INDEX IDX_DFEC3F3938248176 (currency_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE currency (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(100) NOT NULL, code CHAR(3) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE rate (id INT AUTO_INCREMENT NOT NULL, currency_id INT NOT NULL, buy_value NUMERIC(8, 4) NOT NULL, sale_value NUMERIC(8, 4) NOT NULL, created_at DATETIME NOT NULL, INDEX IDX_DFEC3F3938248176 (currency_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE rate ADD CONSTRAINT FK_DFEC3F3938248176 FOREIGN KEY (currency_id) REFERENCES currency (id)');
     }
 
@@ -25,8 +24,6 @@ class Version20180216143849 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE rate DROP FOREIGN KEY FK_DFEC3F3938248176');
         $this->addSql('DROP TABLE rate');
-        $this->addSql('DROP TABLE currency');
     }
 }
