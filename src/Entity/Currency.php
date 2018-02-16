e810a8362f0a3592b0c08de37899a480b80cfdf8
aayaresko <aayaresko@gmail.com>
2018-02-16 16:22:45 +0200

    getters and setters added.

--- src/Entity/Currency.php
+++ src/Entity/Currency.php
@@ -24,5 +24,35 @@ class Currency
      */
     private $code;
 
-    // add your own fields
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * @param mixed $code
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+    }
 }
