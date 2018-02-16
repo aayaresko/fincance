0e52e4f3a581aad2f92e9e87a6716fab4e4167e9
aayaresko <aayaresko@gmail.com>
2018-02-16 21:03:14 +0200

    rates diagram view and controller updated.

--- src/Controller/RateDiagramController.php
+++ src/Controller/RateDiagramController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Currency;
 use App\Entity\Rate;
+use App\Repository\CurrencyRepository;
 use App\Repository\RateRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
@@ -12,17 +14,23 @@ use Symfony\Component\HttpFoundation\Response;
 class RateDiagramController extends Controller
 {
     /**
-     * @Route("/rate/diagram", name="rate_diagram")
+     * @Route("/rate/diagram/{currencyId}", name="rate_diagram")
      * @Method("GET")
      */
-    public function index()
+    public function index($currencyId)
     {
         /**
+         * @var CurrencyRepository $currencyRepository
          * @var RateRepository $ratesRepository
          */
-        $ratesRepository = $this->getDoctrine()->getRepository(Rate::class);
-        $rates           = $ratesRepository->findAll();
+        $currencyRepository = $this->getDoctrine()->getRepository(Currency::class);
+        $ratesRepository    = $this->getDoctrine()->getRepository(Rate::class);
+        $currency           = $currencyRepository->find($currencyId);
+        if (!$currency) {
+            throw $this->createNotFoundException();
+        }
+        $rates = $ratesRepository->findByCurrency($currency);
 
-        return $this->render('rates/diagram/index.html.twig', compact('rates'));
+        return $this->render('rates/diagram/index.html.twig', compact('rates', 'currency'));
     }
 }
