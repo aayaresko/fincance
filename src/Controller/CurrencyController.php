717eeb8cbdace3199f858263592e21394424910f
aayaresko <aayaresko@gmail.com>
2018-02-16 20:49:49 +0200

    added Currency controller and view.

---
+++ src/Controller/CurrencyController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Currency;
+use App\Repository\CurrencyRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class CurrencyController extends Controller
+{
+    /**
+     * @Route("/currency", name="currency")
+     */
+    public function index()
+    {
+        /**
+         * @var CurrencyRepository $currencyRepository
+         */
+        $currencyRepository = $this->getDoctrine()->getRepository(Currency::class);
+        $entities           = $currencyRepository->findAll();
+
+        return $this->render('currency/index.html.twig', compact('entities'));
+    }
+}
