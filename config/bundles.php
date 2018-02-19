1df3c26b5a5a0b73598947169404ffc345dd87f8
aayaresko <aayaresko@gmail.com>
2018-02-19 13:31:18 +0200

    mailer added.

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
 ];
