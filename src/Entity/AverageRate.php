b0d62384d6f3b724b627c70a3eea4c624494b324
aayaresko <aayaresko@gmail.com>
2018-02-19 20:40:40 +0200

    added AverageRate service, repository a d entity.

---
+++ src/Entity/AverageRate.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Entity;
+
+use App\Service\AverageRateService;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\AverageRateRepository")
+ */
+class AverageRate
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+    /**
+     * @ORM\Column(type="smallint")
+     */
+    private $type;
+    /**
+     * @ORM\Column(type="decimal", precision=8, scale=4, nullable=false)
+     */
+    private $value;
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Currency", inversedBy="rates")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $currency;
+    /**
+     * @Gedmo\Timestampable(on="create")
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param mixed $type
+     */
+    public function setType($type)
+    {
+        if ($type === AverageRateService::TYPE_SALE || $type === AverageRateService::TYPE_BUY) {
+            $this->type = $type;
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param mixed $value
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @return Currency
+     */
+    public function getCurrency()
+    {
+        return $this->currency;
+    }
+
+    /**
+     * @param Currency $currency
+     */
+    public function setCurrency(Currency $currency)
+    {
+        $this->currency = $currency;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+}
---
+++ src/Service/AverageRateService.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Service;
+
+
+class AverageRateService
+{
+    const TYPE_SALE = 1;
+    const TYPE_BUY = 2;
+}
