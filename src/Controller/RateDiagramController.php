0e54b087d0cf601d278971f2c9a94b72ee0912f4
aayaresko <aayaresko@gmail.com>
2018-02-19 12:24:38 +0200

    added ChartJs.

--- src/Controller/RateDiagramController.php
+++ src/Controller/RateDiagramController.php
@@ -3,9 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Currency;
+use App\Entity\Organization;
 use App\Entity\Rate;
 use App\Repository\CurrencyRepository;
+use App\Repository\OrganizationRepository;
 use App\Repository\RateRepository;
+use App\Service\ChartJsService;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -20,17 +23,55 @@ class RateDiagramController extends Controller
     public function index($currencyId)
     {
         /**
+         * @var ChartJsService $chartJsService
          * @var CurrencyRepository $currencyRepository
          * @var RateRepository $ratesRepository
+         * @var OrganizationRepository $organizationRepository
          */
-        $currencyRepository = $this->getDoctrine()->getRepository(Currency::class);
-        $ratesRepository    = $this->getDoctrine()->getRepository(Rate::class);
-        $currency           = $currencyRepository->find($currencyId);
+        $chartJsService         = $this->get(ChartJsService::class);
+        $currencyRepository     = $this->getDoctrine()->getRepository(Currency::class);
+        $ratesRepository        = $this->getDoctrine()->getRepository(Rate::class);
+        $organizationRepository = $this->getDoctrine()->getRepository(Organization::class);
+        $currency               = $currencyRepository->find($currencyId);
+        $organizations          = $organizationRepository->findAll();
         if (!$currency) {
             throw $this->createNotFoundException();
         }
-        $rates = $ratesRepository->findByCurrency($currency);
+        /** @var Currency $currency */
+        $data       = [];
+        $labels     = [];
+        $chartLabel = sprintf('Chart for currency %s', $currency->getName());
+        foreach ($organizations as $organization) {
+            /** @var Organization $organization */
+            $set   = $chartJsService->buildDataSet($organization->getTitle(), []);
+            $rates = $ratesRepository
+                ->findBy(
+                    [
+                        'organization' => $organization,
+                        'currency'     => $currency,
+                    ],
+                    [
+                        'createdAt' => 'ASC'
+                    ]
+                );
+            foreach ($rates as $rate) {
+                /** @var Rate $rate */
+                $labels[]               = $rate->getCreatedAt()->format('Y-m-d');
+                $set['data'][]          = $rate->getSaleValue();
+                $set['backgroundColor'] = $chartJsService->getRandomColor();
+                $set['borderColor']     = $chartJsService->getRandomColor();
+            }
+            $data[] = $set;
+        }
 
-        return $this->render('rates/diagram/index.html.twig', compact('rates', 'currency'));
+        return $this->render(
+            'rates/diagram/index.html.twig',
+            [
+                'chartLabel' => $chartLabel,
+                'labels'     => $chartJsService->prepareItems($labels),
+                'data'       => json_encode($data),
+                'currency'   => $currency,
+            ]
+        );
     }
 }
